fa53212935625442de881ebd5c9731a6d0e540b1
sevinc <bd84hdbqx@code.edu.az>
2023-12-12 05:43:47 -0800

    fg

---
+++ New folder (12)/app.js
@@ -0,0 +1,24 @@
+console.log("salam");
+console.log("abc");
+var num = 8;
+console.log(num);
+var word = "salam";
+console.log(word);
+var obj = {
+    name: "sevinc",
+    id: 1,
+};
+console.log(obj);
+var arr = ["a", "b"];
+console.log(arr);
+var arr2 = [
+    {
+        name: "sevinc",
+        id: 1,
+    },
+    {
+        name: "aaaa",
+        id: 2,
+    },
+];
+console.log(arr2);
---
+++ New folder (12)/app.ts
@@ -0,0 +1,24 @@
+console.log("salam");
+console.log("abc");
+let num: number = 8;
+console.log(num);
+let word: string = "salam";
+console.log(word);
+let obj: object = {
+  name: "sevinc",
+  id: 1,
+};
+console.log(obj);
+let arr: string[] = ["a", "b"];
+console.log(arr);
+let arr2: object[] = [
+  {
+    name: "sevinc",
+    id: 1,
+  },
+  {
+    name: "aaaa",
+    id: 2,
+  },
+];
+console.log(arr2);
